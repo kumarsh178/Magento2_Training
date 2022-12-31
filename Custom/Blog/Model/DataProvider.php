cdba932ec3652ac9b6b48413ebca4c40d36cd972
Shailendra Kumar <kumarsh178@gmail.com>
2022-12-31 06:56:45 +0530

    Ui Component form

---
+++ Custom/Blog/Model/DataProvider.php
@@ -0,0 +1,39 @@
+<?php
+namespace Custom\Blog\Model;
+ 
+use Custom\Blog\Model\ResourceModel\Post\CollectionFactory;
+ 
+class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
+{
+
+	protected $collection;
+    /**
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param CollectionFactory $postCollectionFactory
+     * @param array $meta
+     * @param array $data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $postCollectionFactory,
+        array $meta = [],
+        array $data = []
+    ) {
+        $this->collection = $postCollectionFactory->create();
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+    }
+ 
+    /**
+     * Get data
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        return [];
+    }
+}
