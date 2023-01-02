cfc195d6df917b1ab381e480a767e039469fdcd6
Shailendra Kumar <kumarsh178@gmail.com>
2023-01-02 21:48:03 +0530

    Save Ui Component form data

---
+++ Custom/Blog/Controller/Adminhtml/Post/Savepost.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Custom\Blog\Controller\Adminhtml\Post;
+
+class Savepost extends \Magento\Backend\App\Action
+{
+	protected $resultPageFactory = false;
+	protected $postFactory;
+
+	public function __construct(
+		\Magento\Backend\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $resultPageFactory,
+		\Custom\Blog\Model\PostFactory $postFactory
+	)
+	{
+		$this->postFactory = $postFactory;
+		parent::__construct($context);
+		$this->resultPageFactory = $resultPageFactory;
+	}
+
+	public function execute()
+	{
+		$data = $this->getRequest()->getPostValue();
+      /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+	     try{
+	     /** @var \Magento\Cms\Model\Page $model */
+	           $model = $this->postFactory->create();
+			   $model->setData($data);
+			   $model->save();
+		    	$this->messageManager->addSuccessMessage(__('You saved the post.'));
+			}catch(\Exception $e){
+				 $this->messageManager->addExceptionMessage($e, __('Something went wrong while saving the post.'));
+			}
+	 return $resultRedirect->setPath('*/*/');
+	}
+
+
+}
