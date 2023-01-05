55337bbda6ab62306d18020166d327dfc67e6ffb
Shailendra Kumar <kumarsh178@gmail.com>
2023-01-05 10:38:40 +0530

    Admin_GridOrCrud

--- Custom/Blog/Model/Post.php
+++ Custom/Blog/Model/Post.php
@@ -1,6 +1,6 @@
 <?php
 namespace Custom\Blog\Model;
-class Post extends \Magento\Framework\Model\AbstractModel implements \Magento\Framework\DataObject\IdentityInterface
+class Post extends \Magento\Framework\Model\AbstractModel
 {
 	protected function _construct()
 	{
