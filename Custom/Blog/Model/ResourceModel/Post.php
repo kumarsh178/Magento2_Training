3d54ed1f6ddbda794f78154a5616f7812444f211
Shk Ku <shailu@sistena.co.uk>
2023-08-02 11:13:06 +0530

    Magento2 Graphql

---
+++ Custom/Blog/Model/ResourceModel/Post.php
@@ -0,0 +1,20 @@
+<?php
+namespace Custom\Blog\Model\ResourceModel;
+
+
+class Post extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
+{
+	
+	public function __construct(
+		\Magento\Framework\Model\ResourceModel\Db\Context $context
+	)
+	{
+		parent::__construct($context);
+	}
+	
+	protected function _construct()
+	{
+		$this->_init('custom_blog_post', 'post_id');
+	}
+	
+}
